67f50a02761bcacd91aed0dd44ae08c5f9c7d7c3
rafael.lima <rafael.lima@archoffice.tech>
2021-11-05 16:57:32 -0400

    Add calculos ao simulador

--- cadproj/templatetags/simulador.py
+++ cadproj/templatetags/simulador.py
@@ -5,19 +5,20 @@ register = template.Library()
 
 
 @register.simple_tag
-def retorno():
-    investimento = 90
-    valor_do_projeto = Projeto.valor_projeto
-    risco = Projeto.risco
+def retorno(pk, valor):
+    projeto = Projeto.objects.get(id=pk)
+    investimento = int(valor)
+    valor_do_projeto = projeto.valor_projeto
+    risco = projeto.risco
     if investimento < valor_do_projeto:
         investimento = 0
-        return investimento
+        return str(investimento)
     if risco == 0:
         investimento = investimento * 0.05
-        return investimento
+        return str(investimento)
     if risco == 1:
         investimento = investimento * 0.10
-        return investimento
+        return str(investimento)
     if risco == 2:
         investimento = investimento * 0.20
-        return investimento
+        return str(investimento)
