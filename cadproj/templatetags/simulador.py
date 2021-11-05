e36da20723afe29de4c473bf883be98a37403580
rafael.lima <rafael.lima@archoffice.tech>
2021-11-05 13:45:01 -0400

    simulador

---
+++ cadproj/templatetags/simulador.py
@@ -0,0 +1,23 @@
+from cadproj.models import Projeto
+from django import template
+
+register = template.Library()
+
+
+@register.simple_tag
+def retorno_investimento(valor):
+    investimento = valor
+    valor_do_projeto = Projeto.valor_projeto
+    risco = Projeto.risco
+    if investimento < valor_do_projeto:
+        investimento = 0
+        return investimento
+    if risco == 0:
+        investimento = investimento * 0.05
+        return investimento
+    if risco == 1:
+        investimento = investimento * 0.10
+        return investimento
+    if risco == 2:
+        investimento = investimento * 0.20
+        return investimento
